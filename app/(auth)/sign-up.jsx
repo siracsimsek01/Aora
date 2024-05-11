40749b640442023cc9226d88eaa3b7d623d170e3
Sirac Simsek <88107978+siracsimsek01@users.noreply.github.com>
2024-05-11 02:13:09 +0100

    feat: Add sign-up functionality and integrate with Appwrite API

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
@@ -6,6 +6,8 @@ import { Link } from "expo-router";
 import FormField from "../../components/FormField";
 
 import CustomButton from "../../components/CustomButton";
+
+import { createUser } from "../../lib/appwrite";
 
 const SignUp = () => {
   const [form, setForm] = useState({
@@ -16,7 +18,27 @@ const SignUp = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = () => {};
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert('Error', 'Please fill all fields');
+    };
+
+    setIsSubmitting(true);
+
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+
+      // set it to global state
+      router.replace('/home')
+
+    } catch (error) {
+      Alert.alert('Error', error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+
+    createUser();
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -54,7 +76,7 @@ const SignUp = () => {
           />
 
           <CustomButton
-            title="Sign In"
+            title="Sign Up"
             handlePress={submit}
             containerStyles="mt-7"
             isLoading={isSubmitting}

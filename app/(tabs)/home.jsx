13c27e18f093a815298b63c35f0a0ab202b3fd10
Sirac Simsek <88107978+siracsimsek01@users.noreply.github.com>
2024-05-18 21:33:51 +0100

    feat: Update component imports and dependencies

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../../constants";
+import SearchInput from "../../components/SearchInput";
+
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={[]}
+        keyExtractor={(item) => item.$id}
+        renderItem={() => <Text className="text-3xl">{item.id}</Text>}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">Welcome Back</Text>
+                <Text className="text-2xl font-psemibold text-white">Simsek Dev</Text>
+              </View>
 
-export default Home
+              <View className="mt-1.5">
+                <Image
+                source={images.logoSmall}
+                className="w-9 h-10"
+                resizeMode="contain"
+                />
+              </View>
+            </View>
+            <SearchInput/>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Home;
--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -1,11 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
+import React, { useState } from 'react'
 
-const SearchInput = () => {
+import { icons } from '../constants'
+
+const SearchInput = ({ title, value, placeholder, handleChangeText, otherStyles, ...props }) => {
+    const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <View>
-      <Text>SearchInput</Text>
-    </View>
+ 
+      <View className="w-full h-16 px-4 bg-black-100 border-2 border-black-200 rounded-xl focus:border-secondary items-center flex-row space-x-4">
+        <TextInput
+          className="flex-1 text-white font-pregular text-base mt-0.5"
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor="#7b7b8b"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === 'Password' && !showPassword}
+        />
+
+        {title === 'Password' && (
+            <TouchableOpacity onPress={() => 
+                setShowPassword(!showPassword)
+            }>
+                <Image source={!showPassword ? icons.eye : icons.eyeHide} className="w-6 h-6" resizeMode="contain"/>
+            </TouchableOpacity>
+        )}
+      </View>
+
   )
 }
 

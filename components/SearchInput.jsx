0e06b43a2bdd614a8463f7276c92ad987962e60a
Sirac Simsek <88107978+siracsimsek01@users.noreply.github.com>
2024-05-23 21:00:16 +0100

    update

--- components/SearchInput.jsx
+++ components/SearchInput.jsx
@@ -10,12 +10,12 @@ const SearchInput = ({ title, value, placeholder, handleChangeText, otherStyles,
  
       <View className="w-full h-16 px-4 bg-black-100 border-2 border-black-200 rounded-xl focus:border-secondary items-center flex-row space-x-4">
         <TextInput
-          className="flex-1 text-white font-pregular text-base mt-0.5"
+          // className="flex-1 text-white font-pregular text-base mt-0.5"
           value={value}
           placeholder={placeholder}
           placeholderTextColor="#7b7b8b"
           onChangeText={handleChangeText}
-          // secureTextEntry={title === 'Password' && !showPassword}
+          secureTextEntry={title === 'Password' && !showPassword}
         />
 
         {title === 'Password' && (
